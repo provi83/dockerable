a1aede6a51ae8d37bff9da38ae11e9b59db226a4
Seb <intelligent@wp.pl>
2023-12-22 10:35:09 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dockerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(1000);
+            
+        }
+    }
+}
